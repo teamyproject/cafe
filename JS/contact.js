126d1434fb1abe2d9eea20d715782c8d355a6a93
mohamedsaba <151614379+mohamedsaba@users.noreply.github.com>
2024-05-01 21:25:52 +0300

    Update contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -1,22 +1,7 @@
 function search() {
-    var input, filter, container, navbar, elements, i, txtValue;
+    var input, filter, navbar, elements, i, txtValue;
     input = document.getElementById("searchbar");
     filter = input.value.toUpperCase();
-    
-    // Container Search
-    container = document.getElementById("container");
-    elements = container.getElementsByTagName("*");
-    for (i = 0; i < elements.length; i++) {
-        if (elements[i].textContent || elements[i].innerText) {
-            txtValue = elements[i].textContent || elements[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                elements[i].style.display = ""; // Match
-            } else {
-                elements[i].style.display = "none"; // No Match
-            }
-        }
-    }
-
     // Navbar Search
     navbar = document.getElementById("navbar");
     elements = navbar.getElementsByTagName("*");
