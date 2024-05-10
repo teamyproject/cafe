b7180a79d05691627ceb10993be6f3a6c2f638c1
mohamedsaba <151614379+mohamedsaba@users.noreply.github.com>
2024-05-10 19:43:49 +0300

    Update MyAccount.js

--- JS/MyAccount.js
+++ JS/MyAccount.js
@@ -1,14 +1,10 @@
-// Load user data from localStorage when the page loads
 window.addEventListener('DOMContentLoaded', () => {
     loadUserData();
 });
 
 function loadUserData() {
-    // Check if user data exists in localStorage
     if (localStorage.getItem('userData')) {
-        // Parse the stored JSON data
         const userData = JSON.parse(localStorage.getItem('userData'));
-        // Update input fields with stored data
         document.getElementById('name').value = userData.name;
         document.getElementById('age').value = userData.age;
         document.getElementById('email').value = userData.email;
@@ -34,26 +30,22 @@ function enableInputFields() {
     document.getElementById('email').disabled = false;
     document.getElementById('address').disabled = false;
 }
-function validation(){
-    if(document.getElementById("name").value == ""
-        || document.getElementById("email").value == ""
-        || document.getElementById("address").value == "")
-        {
-        window.alert("There is Something Missing!");
-        }
+function validation({name, age, email, address}){
+    if(name == "")      window.alert("There is Something Missing!");
+    if(email == "")     window.alert("There is Something Missing!");
+    if(address == "" )  window.alert("There is Something Missing!");
+    if(age == "")       window.alert("There is Something Missing!");
 }
 function submit() {
-    // Get user input data
-    const name = document.getElementById('name').value;
-    const age = document.getElementById('age').value;
-    const email = document.getElementById('email').value;
+    const name    = document.getElementById('name').value;
+    const age     = document.getElementById('age').value;
+    const email   = document.getElementById('email').value;
     const address = document.getElementById('address').value;
 
-    // Store user data in localStorage
     localStorage.setItem('userData', JSON.stringify({ name, age, email, address }));
 
     updateDisplayedUserInfo(name, age, address);
-    validation();
+    validation({name, age, email, address});
 }
 
 function updateDisplayedUserInfo(name, age, address) {
